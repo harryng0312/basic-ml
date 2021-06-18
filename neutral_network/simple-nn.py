93409c7c760fd4db37ac162c1c4a86efe96bf90d
harryng <little.ruan@gmail.com>
2021-06-18 11:53:57 +0700

    simple nn

--- neutral_network/simple-nn.py
+++ neutral_network/simple-nn.py
@@ -18,13 +18,17 @@ class NeuralNetwork:
         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
         d_weights2 = np.dot(self.layer1.T,
                             (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
+        # tmp = (np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
+        #               self.weights2.T) * sigmoid_derivative(self.layer1))
         tmp = (np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
-                      self.weights2.T) * sigmoid_derivative(self.layer1))
+                      self.weights2) * sigmoid_derivative(self.layer1))
         d_weights1 = np.dot(self.input.T, tmp)
 
         # update the weights with the derivative (slope) of the loss function
-        self.weights1 += d_weights1
-        self.weights2 += d_weights2
+        # self.weights1 += d_weights1
+        # self.weights2 += d_weights2
+        self.weights1 = self.weights1 + d_weights1
+        self.weights2 = self.weights2 + d_weights2
 
 
 def sigmoid(S):
@@ -50,7 +54,11 @@ def tanh(S):
 X = np.array([[0, 0, 1, 1], [0, 1, 0, 1], [1, 1, 1, 1]])
 Y = np.array([0, 1, 1, 0])
 nn = NeuralNetwork(X, Y)
-nn.feedforward()
-nn.backprop()
-print(f"output:\n{nn.weights1}\n")
-print(f"W:\n{nn.weights1}\n\n{nn.weights2}")
+# for i in range(0, 1500):
+for i in range(0, 50):
+    nn.feedforward()
+    nn.backprop()
+    print(f"W:\n{nn.weights1}\n\n{nn.weights2}")
+    print("=====")
+
+print(f"output:\n{nn.output}\n")
